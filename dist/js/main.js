cff2f2c899e6848dae781b1edc2ac80c36d3e8e6
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-08-18 18:17:30 -0600

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function () {
+    'use strict';
+
+    var towers = [{
+        disks: []
+    }, {
+        disks: []
+    }, {
+        disks: []
+    }],
+        n = 9;
+
+    initiateGame(n);
+    moveTower(n - 1, 0, 2, 1);
+
+    function initiateGame(height) {
+        for (var x = height - 1; x >= 0; x--) {
+            towers[0].disks.push(x);
+        }
+        console.log(JSON.stringify(towers));
+    }
+
+    function moveDisk(source, dest) {
+        var disk = towers[source].disks.pop();
+        towers[dest].disks.push(disk);
+        console.log(JSON.stringify(towers));
+    }
+
+    function moveTower(diskValue, source, dest, spare) {
+        if (diskValue === 0) {
+            moveDisk(source, dest);
+        } else {
+            moveTower(diskValue - 1, source, spare, dest);
+            moveDisk(source, dest);
+            moveTower(diskValue - 1, spare, dest, source);
+        }
+    }
+})();
+//# sourceMappingURL=main.js.map
